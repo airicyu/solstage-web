f774d8bf4fca15f62624837b86f6ac8d1087e4a1
Airic Yu <airic.yu@gmail.com>
2024-03-31 21:55:18 +0800

    swap trx add priority fee

--- src/app/swap/swap-shdw.tsx
+++ src/app/swap/swap-shdw.tsx
@@ -1,5 +1,18 @@
 import { WalletContextState } from "@solana/wallet-adapter-react";
-import { Connection, VersionedTransaction } from "@solana/web3.js";
+import {
+  AddressLookupTableAccount,
+  ComputeBudgetProgram,
+  Connection,
+  PublicKey,
+  TransactionInstruction,
+  TransactionMessage,
+  VersionedTransaction,
+} from "@solana/web3.js";
+
+const PRIORITY_RATE = 5; // MICRO_LAMPORTS
+const PRIORITY_FEE_IX = ComputeBudgetProgram.setComputeUnitPrice({
+  microLamports: PRIORITY_RATE,
+});
 
 export async function swapSolForShdw(
   wallet: WalletContextState,
@@ -13,12 +26,12 @@ export async function swapSolForShdw(
     await fetch(
       `https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=SHDWyBxihqiCj6YekG2GUr7wqKLeLAMK1gHZck9pL6y&amount=${
         numShdw * 10 ** 9
-      }&slippageBps=10000&swapMode=ExactOut`
+      }&slippageBps=500&swapMode=ExactOut`
     )
   ).json();
 
-  const { swapTransaction } = await (
-    await fetch("https://quote-api.jup.ag/v6/swap", {
+  const instructions = await (
+    await fetch("https://quote-api.jup.ag/v6/swap-instructions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -29,21 +42,84 @@ export async function swapSolForShdw(
         // user public key to be used for the swap
         userPublicKey: wallet.publicKey.toString(),
         // auto wrap and unwrap SOL. default is true
-        wrapAndUnwrapSol: true,
         // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
         // feeAccount: "fee_account_public_key"
       }),
     })
   ).json();
 
-  const swapTransactionBuf = Buffer.from(swapTransaction, "base64");
-  const transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-  // console.log(transaction);
+  if (instructions.error) {
+    throw new Error("Failed to get swap instructions: " + instructions.error);
+  }
 
-  // // sign the transaction
+  const {
+    // tokenLedgerInstruction, // If you are using `useTokenLedger = true`.
+    // computeBudgetInstructions, // The necessary instructions to setup the compute budget.
+    setupInstructions, // Setup missing ATA for the users.
+    swapInstruction: swapInstructionPayload, // The actual swap instruction.
+    cleanupInstruction, // Unwrap the SOL if `wrapAndUnwrapSol = true`.
+    addressLookupTableAddresses, // The lookup table addresses that you can use if you are using versioned transaction.
+  } = instructions;
+
+  const deserializeInstruction = (instruction) => {
+    return new TransactionInstruction({
+      programId: new PublicKey(instruction.programId),
+      keys: instruction.accounts.map((key) => ({
+        pubkey: new PublicKey(key.pubkey),
+        isSigner: key.isSigner,
+        isWritable: key.isWritable,
+      })),
+      data: Buffer.from(instruction.data, "base64"),
+    });
+  };
+
+  const getAddressLookupTableAccounts = async (
+    keys: string[]
+  ): Promise<AddressLookupTableAccount[]> => {
+    const addressLookupTableAccountInfos =
+      await connection.getMultipleAccountsInfo(
+        keys.map((key) => new PublicKey(key))
+      );
+
+    return addressLookupTableAccountInfos.reduce((acc, accountInfo, index) => {
+      const addressLookupTableAddress = keys[index];
+      if (accountInfo) {
+        const addressLookupTableAccount = new AddressLookupTableAccount({
+          key: new PublicKey(addressLookupTableAddress),
+          state: AddressLookupTableAccount.deserialize(accountInfo.data),
+        });
+        acc.push(addressLookupTableAccount);
+      }
+
+      return acc;
+    }, new Array<AddressLookupTableAccount>());
+  };
+
+  const addressLookupTableAccounts: AddressLookupTableAccount[] = [];
+
+  addressLookupTableAccounts.push(
+    ...(await getAddressLookupTableAccounts(addressLookupTableAddresses))
+  );
+
+  const latestBlockhash = await connection.getLatestBlockhash();
+  const messageV0 = new TransactionMessage({
+    payerKey: wallet.publicKey,
+    recentBlockhash: latestBlockhash.blockhash,
+    instructions: [
+      PRIORITY_FEE_IX,
+      ...setupInstructions.map(deserializeInstruction),
+      deserializeInstruction(swapInstructionPayload),
+      deserializeInstruction(cleanupInstruction),
+    ],
+  }).compileToV0Message(addressLookupTableAccounts);
+
+  const transaction = new VersionedTransaction(messageV0);
   const tx = await wallet.sendTransaction(transaction, connection);
 
-  await connection.confirmTransaction(tx, "confirmed");
+  await connection.confirmTransaction(
+    { signature: tx, ...latestBlockhash },
+    "confirmed"
+  );
 
   return tx;
 }
